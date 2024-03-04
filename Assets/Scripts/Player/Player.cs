aa8d86c92f05b048ccae99d5fb5f1491ebbdef82
drakonaft12 <61424327+drakonaft12@users.noreply.github.com>
2024-03-04 21:40:11 +0300

    More fixed

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -13,12 +13,12 @@ public class Player : MonoBehaviour
     Rigidbody rigidbodyPlayer;
     CapsuleCollider capsule;
     [SerializeField] Animator animator;
-    [SerializeField] Camera cameraPl;
+    [SerializeField] GameObject cameraPl;
     [SerializeField] TriggerUp triggerUp;
     [SerializeField] TriggerJump triggerJump;
     bool isCtrl = false;
-    Vector3 vector;
-    float su;
+    Vector3 vector, vecUp;
+    float su, visotaStolknoveni = 0;
 
     private void Awake()
     {
@@ -32,34 +32,48 @@ public class Player : MonoBehaviour
         animator.SetFloat("Horizontal", move.x);
         animator.SetFloat("Vertical", move.z+sprint);
         var az = math.abs(move.x);
+        var normMove = move.normalized;
         animator.SetFloat("VelVert", move.z!=0? move.z + sprint:az);
-        vector = transform.TransformDirection(move.normalized)*4;
+        vector = transform.TransformDirection(normMove) *4;
         
         float maxspeed;
-        if (isCtrl) { maxspeed = PlayerStats.me.speed * 0.5f; }
+        if (isCtrl) { maxspeed = PlayerStats.me.speed /2; }
         else if (sprint !=0&& !triggerUp.isUstal) { maxspeed = PlayerStats.me.sprintSpeed* sprint; maxspeed = maxspeed < PlayerStats.me.speed ? PlayerStats.me.speed : maxspeed; }
         else { maxspeed = PlayerStats.me.speed; }
+        var deltaSpeed = rigidbodyPlayer.velocity / maxspeed;
+        vector -= deltaSpeed*4;
+        vector /= 4;
+
         maxspeed /= 4;
-        var velosityaff = new Vector2(rigidbodyPlayer.velocity.x, rigidbodyPlayer.velocity.z).magnitude/ maxspeed / maxspeed;
-        vector -= rigidbodyPlayer.velocity / maxspeed;
-       
-        vector.y = rigidbodyPlayer.velocity.y;
-        var si = move.normalized.magnitude - velosityaff;
-        si = si <= 0 ? 0 : si;
-        su = si;
+        vector.y = 0;
+        var velosityaff = new Vector2(rigidbodyPlayer.velocity.x, rigidbodyPlayer.velocity.z).magnitude / maxspeed;
+        su = normMove.magnitude - velosityaff;
+        su = su <= 0 ? 0 : su;
+        float v = animator.GetFloat("Visota");
         if (!triggerJump.isJump && !triggerUp.isTrogat) { vector.x *= 0.1f; vector.z *= 0.1f; }
-        else if (triggerUp.isTrogat) { 
-            transform.position += new Vector3(0, (si)* move.z * maxspeed* PlayerStats.me.zalesSpeed * Time.deltaTime);
-             }
+        else if (triggerUp.isTrogat) {
+            var spUp = (su) * move.z * maxspeed * PlayerStats.me.zalesSpeed;
+            vecUp = new Vector3(0, spUp, 0);
+            if (v < 0.55f) { vector.y = vecUp.y; }
+            else
+            transform.position += vecUp * Time.deltaTime;
+        }
 
         float summ = (sprint * PlayerStats.me.staminaSprint) + (triggerUp.isTrogat&& !triggerJump.isJump ? PlayerStats.me.staminaZalesanie : 0);
         triggerUp.StaminaUpdate(!triggerUp.isUstal ? move.magnitude* summ * maxspeed / 5:0);
         
-        vector.y -= rigidbodyPlayer.velocity.y;
+  
 
         rigidbodyPlayer.AddForce(vector, ForceMode.VelocityChange);
+
         animator.SetBool("isJump", !triggerJump.isJump);
+
+        animator.SetBool("isTrogat", triggerUp.isTrogat && visotaStolknoveni > 0.1f);
+        
+        if (triggerUp.isUstal) v = 0;
+        animator.SetFloat("Visota", v + (visotaStolknoveni - v) * 0.4f);
     }
+
 
     public void AnimRead(bool iz)
     {
@@ -68,41 +82,42 @@ public class Player : MonoBehaviour
 
     private void OnCollisionStay(Collision collision)
     {
-        if (triggerUp.isTrogat&& su > 0 && !triggerJump.isJump)
+        if (triggerUp.isTrogat && su > 0 && !triggerJump.isJump) 
         {
             var p = collision.GetContact(0);
             triggerUp.PovorotModel(p);
             
         }
-        
-        var vis = collision.GetContact(0).point.y - transform.position.y +1;
-        vis *= 2;
-        animator.SetBool("isTrogat", triggerUp.isTrogat && vis>0.1f);
-        float v = animator.GetFloat("Visota");
-        animator.SetFloat("Visota", v + (vis - v) * 0.3f);
-        Debug.Log(vis);
+
+        visotaStolknoveni = collision.GetContact(0).point.y - transform.position.y +1;
+        visotaStolknoveni *= 3;
     }
 
     private bool oneJump = false;
     public void JumpPlayer(float jump)
     {
+        animator.SetInteger("Jump", 1);
+        if (jump!=0) animator.SetInteger("Jump", 2);
         if (jump != 0 && !oneJump)
         {
-            animator.SetInteger("Jump", 1);
+
+            
             vector = rigidbodyPlayer.velocity;
             if (triggerJump.isJump&& !triggerUp.isUstal) {
                 
                 vector.y =vector.y*0.1f+ jump * PlayerStats.me.heithJump; 
                 triggerUp.StaminaUpdate(PlayerStats.me.staminaJump);
                 oneJump = true;
-                
             }
             rigidbodyPlayer.velocity = vector;
             
         }
-        if (!triggerJump.isJump) oneJump = false;
-        animator.SetInteger("Jump", 2);
-        if (triggerJump.isJump) { animator.SetInteger("Jump", 3); }
+        if (!triggerJump.isJump)
+        {
+            oneJump = false;
+            
+        }
+        if (triggerJump.isJump && !oneJump) { animator.SetInteger("Jump", 3); }
         
     }
 
